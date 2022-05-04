af2f305d0ca65649660bbdcdeee07b6dea33ebf8
byeongjeon jeon <pensieve92@gmail.com>
2022-05-05 08:43:17 +0900

    Update App page

--- cover-code/src/App.vue
+++ cover-code/src/App.vue
@@ -1,18 +1,18 @@
 <template>
 	<div id="app">
-		<img alt="Vue logo" src="@/assets/logo.png" />
-		<HelloWorld msg="Welcome to Your Vue.js App" />
+		<header>
+			<router-link to="/js">js</router-link>
+			<router-link to="/vue">vue</router-link>
+			<router-link to="/java">java</router-link>
+		</header>
+		<router-view></router-view>
 	</div>
 </template>
 
 <script>
-import HelloWorld from '@/components/HelloWorld.vue';
-
 export default {
 	name: 'App',
-	components: {
-		HelloWorld,
-	},
+	components: {},
 };
 </script>
 
