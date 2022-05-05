d7d0903412d602751ad6d7bb9a159c769d4e85cf
byeongjeon jeon <pensieve92@gmail.com>
2022-05-05 09:02:03 +0900

    Add post components

---
+++ cover-code/src/components/post/Post.vue
@@ -0,0 +1,13 @@
+<template>
+	<!-- 목적:	포스트 상세내용 -->
+	<!-- 내용: 상세 게시글 markdown editor 사용 예정..-->
+	<div>Post</div>
+</template>
+
+<script>
+export default {
+	name: 'Post',
+};
+</script>
+
+<style scoped></style>
