59d21bd2fa99c075fc9b0bf754746bc4c3ce0af8
byeongjeon jeon <pensieve92@gmail.com>
2022-05-05 09:02:03 +0900

    Add Post Component

---
+++ cover-code/src/components/post/PostCard.vue
@@ -0,0 +1,13 @@
+<template>
+	<!-- 목적:	포스트 간략 표시-->
+	<!-- 내용: 제목 + 작성일 + 간단내용-->
+	<div>PostCard</div>
+</template>
+
+<script>
+export default {
+	name: 'PostCard',
+};
+</script>
+
+<style scoped></style>
---
+++ cover-code/src/components/post/PostCardList.vue
@@ -0,0 +1,13 @@
+<template>
+	<!-- 목적:	포스트카드의 리스트 표시 -->
+	<!-- 내용: PostCard 컴포넌트를 10개씩 뿌려준다.-->
+	<div>PostCardList</div>
+</template>
+
+<script>
+export default {
+	name: 'PostCardList',
+};
+</script>
+
+<style scoped></style>
